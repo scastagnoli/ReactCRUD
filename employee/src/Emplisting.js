681b0c185a06333bf20b8462d08373688b260cc9
scastagnoli <scastagnoli@users.noreply.github.com>
2023-03-05 18:23:26 +0100

    Add files via upload

---
+++ employee/src/Emplisting.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Emplisting() {
+  
+  const [empdata, empdatachange] = useState(null);
+
+  const navigate=useNavigate();
+
+  const LoadDetail = (id) => {
+    navigate("/employee/detail/"+id)
+  };
+
+  const LoadEdit = (id) => {
+    navigate("/employee/edit/"+id)
+  };
+
+  const RemoveFunction = (id) => {
+    if(window.confirm('Do you confirm?')) {
+        fetch("http://localhost:8000/employee/" + id, {
+      method: "DELETE"
+    })
+      .then((res) => {
+        alert("Removed succefully!");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }
+  };
+
+
+  useEffect(() => {
+    fetch("http://localhost:8000/employee")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        empdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>Emplyee Listing</h2>
+        </div>
+        <div className="card-body">
+          <div className="divbtn">
+            <div>
+              <Link to="/employee/create" className="btn btn-success">
+                Add New (+)
+              </Link>
+            </div>
+          </div>
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {empdata &&
+                empdata.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>
+                      <a
+                        onClick={() => {
+                          LoadEdit(item.id);
+                        }}
+                        className="btn btn-success"
+                      >
+                        Edit
+                      </a>
+                      <a
+                        onClick={() => {
+                          RemoveFunction(item.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        Remove
+                      </a>
+                      <a
+                        onClick={() => {
+                          LoadDetail(item.id);
+                        }}
+                        className="btn btn-primary"
+                      >
+                        Detail
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Emplisting;
